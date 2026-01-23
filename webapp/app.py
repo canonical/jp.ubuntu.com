0b3e4dd008c4fee02297139a92a63b547835665d
Madhur Jain <21112002mj@gmail.com>
2026-01-23 12:13:55 +0530

    rebranded 404 and 500 pages

--- webapp/app.py
+++ webapp/app.py
@@ -56,13 +56,14 @@ def get_cache(key):
 def set_cache(key, value, timeout):
     cache.set(key, value, timeout)
 
-
-cookie_service = CookieConsent().init_app(
-    app,
-    get_cache_func=get_cache,
-    set_cache_func=set_cache,
-    start_health_check=True,
-)
+cookie_service = None
+if not app.debug:
+    cookie_service = CookieConsent().init_app(
+        app,
+        get_cache_func=get_cache,
+        set_cache_func=set_cache,
+        start_health_check=True,
+    )
 
 
 blog_views = BlogViews(
