84f1139e2d56f1b5127ad368c464c786817df11f
Benjamin Oni <onibenjo@gmail.com>
2026-01-28 14:56:45 +0100

    chore: missing charts and slider, js functionality

--- webpack.config.entry.js
+++ webpack.config.entry.js
@@ -1,4 +1,7 @@
 module.exports = {
   ["latest-news"]: "./static/js/latest-news.js",
   ["global-nav"]: "./static/js/global-nav.js",
+  ["accordion"]: "./static/js/accordion.js",
+  ["cloud-price-slider"]: "./static/js/cloud-price-slider.js",
+  ["openstack-deployment-chart"]: "./static/js/openstack-deployment-chart.js",
 };
