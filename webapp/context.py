831179114bdf0ffa4bc31a5544ea398bb0614ec5
Atakan Sağlam <atakan.saglam@canonical.com>
2026-05-01 11:31:40 +0300

    feat: Rebrand engage index page

---
+++ webapp/context.py
@@ -0,0 +1,13 @@
+# Standard library
+from urllib.parse import parse_qs, urlencode
+
+# Packages
+import flask
+
+def modify_query(params):
+    query_params = parse_qs(
+        flask.request.query_string.decode("utf-8"), keep_blank_values=True
+    )
+    query_params.update(params)
+
+    return urlencode(query_params, doseq=True)
