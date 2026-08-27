a06b1b04e112e1d8fb116652c73c819a0706b5c5
Benjamin Oni <onibenjo@gmail.com>
2026-08-27 15:47:46 +0100

    fix: remove unused modify_query function from app.py

--- webapp/app.py
+++ webapp/app.py
@@ -235,16 +235,6 @@ def download_releases():
 app.add_url_rule("/download", view_func=download_releases)
 
 
-# Blog pagination
-def modify_query(params):
-    query_params = parse_qs(
-        flask.request.query_string.decode("utf-8"), keep_blank_values=True
-    )
-    query_params.update(params)
-
-    return urlencode(query_params, doseq=True)
-
-
 # Image template
 @app.context_processor
 def utility_processor():
